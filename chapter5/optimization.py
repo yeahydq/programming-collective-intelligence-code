23ecc6f827a1ccd9663ff89a3eec1c5514799a59
clasnake <classical.zhu@gmail.com>
2014-02-05 20:55:54 +0800

    Bug fix of printschedule and schedulecost.

--- chapter5/optimization.py
+++ chapter5/optimization.py
@@ -28,8 +28,8 @@ def printschedule(r):
   for d in range(len(r)/2):
     name=people[d][0]
     origin=people[d][1]
-    out=flights[(origin,destination)][int(r[d])]
-    ret=flights[(destination,origin)][int(r[d+1])]
+    out=flights[(origin,destination)][int(r[d*2])]
+    ret=flights[(destination,origin)][int(r[d*2+1])]
     print '%10s%10s %5s-%5s $%3s %5s-%5s $%3s' % (name,origin,
                                                   out[0],out[1],out[2],
                                                   ret[0],ret[1],ret[2])
@@ -42,8 +42,8 @@ def schedulecost(sol):
   for d in range(len(sol)/2):
     # Get the inbound and outbound flights
     origin=people[d][1]
-    outbound=flights[(origin,destination)][int(sol[d])]
-    returnf=flights[(destination,origin)][int(sol[d+1])]
+    outbound=flights[(origin,destination)][int(sol[d*2])]
+    returnf=flights[(destination,origin)][int(sol[d*2+1])]
     
     # Total price is the price of all outbound and return flights
     totalprice+=outbound[2]
@@ -58,8 +58,8 @@ def schedulecost(sol):
   totalwait=0  
   for d in range(len(sol)/2):
     origin=people[d][1]
-    outbound=flights[(origin,destination)][int(sol[d])]
-    returnf=flights[(destination,origin)][int(sol[d+1])]
+    outbound=flights[(origin,destination)][int(sol[d*2])]
+    returnf=flights[(destination,origin)][int(sol[d*2+1])]
     totalwait+=latestarrival-getminutes(outbound[1])
     totalwait+=getminutes(returnf[0])-earliestdep  
 
